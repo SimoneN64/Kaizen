bef4705ffadd0752690e6798793c83d5229a164e
Simone Coco <simone@midorisrl.eu>
2022-09-17 21:40:38 +0200

    avoid building tests and archs other than MIPS for capstone in CI

--- src/frontend/App.cpp
+++ src/frontend/App.cpp
@@ -31,7 +31,7 @@ void App::Run() {
             case SDLK_o: {
               nfdchar_t* outpath;
               const nfdu8filteritem_t filter {"Nintendo 64 roms", "n64,z64,v64,N64,Z64,V64"};
-              nfdresult_t result = NFD_OpenDialog(&outpath, &filter, 1, "/run/media/simuuz/HDD/n64_roms/tests");
+              nfdresult_t result = NFD_OpenDialog(&outpath, &filter, 1, nullptr);
               if(result == NFD_OKAY) {
                 core.LoadROM(outpath);
                 NFD_FreePath(outpath);

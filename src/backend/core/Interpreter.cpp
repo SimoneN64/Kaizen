53b86b1a602dbebe653942412b3e0d3fead6f9a4
SimoneN64 <simococo03@gmail.com>
2023-09-07 14:57:09 +0200

    Exceptions *do* take some cycles, at least 1 for now

--- src/backend/core/Interpreter.cpp
+++ src/backend/core/Interpreter.cpp
@@ -11,14 +11,14 @@ int Interpreter::Step() {
   if(!MapVAddr(regs, LOAD, regs.pc, paddr)) {
     HandleTLBException(regs, regs.pc);
     FireException(regs, GetTLBExceptionCode(regs.cop0.tlbError, LOAD), 0, false);
-    return 0;
+    return 1;
   }
 
   u32 instruction = mem.Read32(regs, paddr);
 
   if(ShouldServiceInterrupt()) {
     FireException(regs, ExceptionCode::Interrupt, 0, false);
-    return 0;
+    return 1;
   }
 
   regs.oldPC = regs.pc;

d3df933ca25732beab18208c28f148c0fcfec0f7
SimoneN64 <simococo03@gmail.com>
2024-09-23 10:05:58 +0200

    Fix Qt bug

--- src/frontend/Debugger.cpp
+++ src/frontend/Debugger.cpp
@@ -2,15 +2,24 @@
 #include <QGuiApplication>
 
 Debugger::Debugger() : QWidget(nullptr) {
-  disassembly = new QDockWidget(this);
+  disassembly = new QDockWidget;
   disassembly->setWindowTitle("Disassembly");
   disassembly->setFeatures(QDockWidget::DockWidgetFloatable | QDockWidget::DockWidgetMovable);
   codeView = new QTreeView(disassembly);
   codeView->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
   codeView->setHeaderHidden(true);
-  cpuState = new QDockWidget(this);
+  cpuState = new QDockWidget;
   cpuState->setWindowTitle("Registers");
   cpuState->setFeatures(QDockWidget::DockWidgetFloatable | QDockWidget::DockWidgetMovable);
   registers = new QTreeView(cpuState);
   registers->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+  horLayout = new QHBoxLayout;
+  horLayout->addWidget(disassembly);
+  horLayout->addWidget(cpuState);
+
+  verLayout = new QVBoxLayout;
+  verLayout->addLayout(horLayout);
+
+  setLayout(verLayout);
 }
--- src/frontend/Debugger.hpp
+++ src/frontend/Debugger.hpp
@@ -2,10 +2,14 @@
 #include <QDockWidget>
 #include <QWidget>
 #include <QTreeView>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 
 class Debugger : public QWidget {
   QDockWidget *disassembly{}, *cpuState{};
   QTreeView *codeView{}, *registers{};
+  QHBoxLayout *horLayout{};
+  QVBoxLayout *verLayout{};
 
 public:
   Debugger();
